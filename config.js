9fdcd97b07570bc94afbb681dd726e1dc5f3f8bb
akadagala <52013492+akadagala@users.noreply.github.com>
2023-07-26 04:45:03 -0400

    Revert "Biggie update"

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    site_port: 5000,
-    database_file: "groceries_app.db",
-    secret: process.env.SECRET != null ? process.env.SECRET : "fakesecret"
-}
-
-module.exports = config;
